83e3a4a8ca3ed4dd8ac5e0b1a7f0691fb4bb6f9a
Francis Mendez <fmendez884@gmail.com>
2019-04-11 17:17:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,15 @@
-def reverse_each_word(string)
-  try = string.split(' ')
-  try.collect do |x| 
-    x.reverse
+def reverse_collect_word(string)
+y = string.split(' ')
+  y.collect do |x| 
+    x.reverse!
   end
+y.join(' ')
+end
+
+def reverse_each_word(string)
+y = string.split(' ')
+  y.each do |x|
+    x.reverse!
+  end
+y.join(' ')
 end
